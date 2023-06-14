11315937d7fba273fb2a3e91e06cdac1e4b306d5
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-06-14 22:17:32 +0200

    Add initial configuration files and test command.

---
+++ commands/testing/test.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('test')
+		.setDescription('Test Command'),
+	async execute(interaction) {
+		await interaction.reply('Primero que todo, cómo están los máquinas? Yo ya estoy conectado');
+	},
+};
