c0ed71473d7e0b900384d7c14832e42aeed98812
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-07-20 22:56:03 +0200

    Modify: Refactor interaction create - Lobby Creation

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -31,48 +31,7 @@ module.exports = {
 				console.log('Lobby Modal submitted');
 				// Create Voice Channel
 				try {
-					// Get Modal Data
-					let category = '';
-					const lobbyName = interaction.fields.getTextInputValue('lobbyNameInput');
-					console.log(`Obtained Lobby: ${lobbyName}`);
-
-					const lobbyPlayers = interaction.fields.getTextInputValue('lobbyPlayersInput');
-					console.log(`Obtained Players: ${lobbyPlayers}`);
-
-					const gameSelected = interaction.fields.getTextInputValue('lobbyGameInput');
-					console.log(`Obtained game: ${gameSelected}`);
-
-					switch (gameSelected) {
-					case 'lol': {
-						category = guild.channels.cache.get(GameCategories.LeagueOfLegends);
-						break;
-					}
-					case 'csgo': {
-						interaction.reply('Not implemented yet');
-						return;
-					}
-					default: {
-						interaction.reply('Unknown game provided');
-						return;
-					}
-					}
-					console.log(`GameCategoryId: ${GameCategories.LeagueOfLegends}`);
-
-					if (category) {
-						await guild.channels.create({
-							name: lobbyName,
-							type: ChannelType.GuildVoice,
-							userLimit:parseInt(lobbyPlayers),
-							parent: category,
-							// TO-DO: Deny access to Everyone role but the user creating the channel and the client.
-						});
-
-						await interaction.reply('SUCCESS');
-					}
-					else {
-						console.error('Voice channel was not created');
-					}
-
+					await CreateVoiceChannel(interaction, guild);
 				}
 				catch (error) {
 					console.error('An error ocurred', error);
@@ -85,3 +44,47 @@ module.exports = {
 		}
 	},
 };
+
+async function CreateVoiceChannel(interaction, guild) {
+	// Get Modal Data
+	let category = '';
+	const lobbyName = interaction.fields.getTextInputValue('lobbyNameInput');
+	console.log(`Obtained Lobby: ${lobbyName}`);
+
+	const lobbyPlayers = interaction.fields.getTextInputValue('lobbyPlayersInput');
+	console.log(`Obtained Players: ${lobbyPlayers}`);
+
+	const gameSelected = interaction.fields.getTextInputValue('lobbyGameInput');
+	console.log(`Obtained game: ${gameSelected}`);
+
+	switch (gameSelected) {
+	case 'lol': {
+		category = guild.channels.cache.get(GameCategories.LeagueOfLegends);
+		break;
+	}
+	case 'csgo': {
+		interaction.reply('Not implemented yet');
+		return;
+	}
+	default: {
+		interaction.reply('Unknown game provided');
+		return;
+	}
+	}
+	console.log(`GameCategoryId: ${GameCategories.LeagueOfLegends}`);
+
+	if (category) {
+		await guild.channels.create({
+			name: lobbyName,
+			type: ChannelType.GuildVoice,
+			userLimit:parseInt(lobbyPlayers),
+			parent: category,
+			// TO-DO: Deny access to Everyone role but the user creating the channel and the client.
+		});
+
+		await interaction.reply('SUCCESS');
+	}
+	else {
+		console.error('Voice channel was not created');
+	}
+}
