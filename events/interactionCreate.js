7ecbbc1e72a9e6374192ec49c68d8a5fd3a2a39a
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-08-05 12:23:30 +0200

    ADD: Notification feature when new lobby is created

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -3,6 +3,7 @@ const {
     InteractionType,
     ChannelType,
     PermissionsBitField,
+    EmbedBuilder,
 } = require('discord.js')
 
 const path = require('path')
@@ -12,12 +13,65 @@ const logger = require('../commons/Logging/winstonLogger')
 const filePath = path.relative(process.cwd.toString(), __dirname)
 
 const GameCategoriesID = require('../commons/enums/gameCategories')
+const NotificationChannelsID = require('../commons/enums/notificationsChannels')
 const {
     handleButtonInteraction,
 } = require('../handlers/queueing/buttonHandler')
 
 function getModalInputValue(interaction, fieldID) {
     return interaction.fields.getTextInputValue(fieldID)
+}
+
+async function sendNotification(interaction, selectedGame, newChannel) {
+    // Feature: Send notification to category's notification channel.
+    // Given the guild member has selected Create Lobby from the menu
+    // When channel is already created
+    // Then an embed message is sent to the notification channel
+    const notificationEmbed = new EmbedBuilder()
+        .setTitle('LOBBY CREATED')
+        .setAuthor({
+            name: interaction.member.displayName,
+            iconURL: interaction.member.displayAvatarURL({
+                forceStatic: false,
+                size: 1024,
+            }),
+        })
+        .setDescription('Looking for players')
+        .setThumbnail(
+            interaction.member.displayAvatarURL({
+                forceStatic: false,
+                size: 2048,
+            })
+        )
+        .addFields(
+            {
+                name: 'Channel',
+                value: `${newChannel.toString()}`,
+                inline: true,
+            },
+            {
+                name: 'Max Players',
+                value: `${newChannel.userLimit}`,
+                inline: true,
+            }
+        )
+        .setTimestamp()
+
+    const notificationChannel =
+        NotificationChannelsID[selectedGame]?.() ??
+        NotificationChannelsID.default()
+
+    if (notificationChannel === 'UNKNOWN') {
+        await interaction.reply({
+            content: 'Notification Channel not found',
+            ephemeral: true,
+        })
+        return
+    }
+
+    interaction.guild.channels.cache
+        .get(notificationChannel)
+        .send({ embeds: [notificationEmbed] })
 }
 
 async function generateLobby(guild, interaction) {
@@ -47,7 +101,7 @@ async function generateLobby(guild, interaction) {
             return
         }
 
-        await guild.channels.create({
+        const newChannel = await guild.channels.create({
             name: lobbyName,
             type: ChannelType.GuildVoice,
             userLimit: parseInt(lobbyPlayers, 10),
@@ -63,6 +117,8 @@ async function generateLobby(guild, interaction) {
                 },
             ],
         })
+
+        await sendNotification(interaction, selectedGame, newChannel)
 
         interaction.reply({
             content: `Channel created in category ${category}`,
