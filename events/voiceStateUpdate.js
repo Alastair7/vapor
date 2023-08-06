3ae8966c53bd32a02e3b0c2f7881dac6832413ae
Alastair7 <alexandru.ban.drmer@gmail.com>
2023-08-06 20:32:01 +0200

    MODIFY: Remove voiceStateUpdate comments

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -12,20 +12,12 @@ module.exports = {
     async execute(oldState, newState) {
         const { channel } = newState
 
-        // Feature: Delete a voice channel when there are no users connected
         if (!channel) return
-        // Scenario: Voice Channel doesn't exist in any category
-        // When: The channels were created by staff and they are public
-        // Then: These channels are not deleted
 
         const channelCategory = channel.parentId
         const isGameCategory = checkCategoryExists(channelCategory)
 
         if (!isGameCategory) return
-        // Scenario: Voice Channel exists in any category
-        // When: Channel was created by member
-        // Then: Channel waits 30 secs for users to connect
-        // Then: Channel is deleted automatically
 
         if (channel.members.size <= 0) {
             setTimeout(async () => {
